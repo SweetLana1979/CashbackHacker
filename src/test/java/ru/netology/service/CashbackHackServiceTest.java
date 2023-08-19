098992dd733dfc362b4461c5afe872a96273e2b0
Neto test <bolsvet190679@mail.ru>
2023-08-19 17:22:10 +0300

    Changes

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemainNone() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+      
+    @org.testng.annotations.Test
+    public void testRemain1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 600;
+        int actual = service.remain(amount);
+        int expected = 400;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testRemain4() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+}
